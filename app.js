f855f27f16c9eeeedf1d1403d4c8f12e96851dd7
nguyendm77 <nguyendm77@gmail.com>
2020-06-22 09:21:35 -0700

    submit code

--- app.js
+++ app.js
@@ -11,13 +11,12 @@ app.get('/', function (req, res) {
     res.render('index', {weather: null, error: null});
   })
 
-app.get('/weather/', function (req, res) {
-    weather.find({search: req.query.zipcode, degreeType: 'F'}, function(err, result) {
-        if(err) {
-            console.log(err);
-            console.log(JSON.stringify(result, null, 2));
-        }
-        res.render('data.ejs', {query: req.query.zipcode, result: result[0]})  
+app.get('/weather', function (req, res) {
+    // console.log(req.query.zipcode);
+    weather.find({search: `${req.query.zipcode}`, degreeType: 'F'}, function(err, result) {
+        if(err) console.log(err);
+        
+        res.render('data', {query: req.query.zipcode, result: result[0]})  
     });
 })
  
